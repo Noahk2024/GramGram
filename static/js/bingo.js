6e6d8592b0059f8b424a85f62fa1dbb745865223
Noahk2024 <2024nkabiri@tjhsst.edu>
2023-03-04 19:36:10 -0500

    Co-authored-by: ojasc19 <ojasc19@users.noreply.github.com> Co-authored-by: Alex-Nassif <Alex-Nassif@users.noreply.github.com> Co-authored-by: Anahi Hernani Vidal <hekqte@users.noreply.github.com> Co-authored-by: alexandernassif1@gmail.com <alexandernassif1@gmail.com>

--- static/js/bingo.js
+++ static/js/bingo.js
@@ -2,6 +2,11 @@ window.onload = function() {
     var table = document.getElementById("bingoTable");
     var reloadLink = document.getElementById("reloadLink");
 
+    var number=76;
+    var used = new Array(76);
+    var digit1 = 0;
+    var digit2 = 0;
+
     // Set up click listener for reload link
     reloadLink.addEventListener("click", function(event) {
         event.preventDefault();
@@ -68,3 +73,4 @@ function resetBoard(table) {
         table.deleteRow(0);
     }
 }
+
